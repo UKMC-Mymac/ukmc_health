5b4f749d2fb7960897eed6c39e28e2da8fda1c7e
Sayed-73 <saidulislam0400@gmail.com>
2026-07-31 17:12:25 +0600

    update

---
+++ app/Models/Contact/Contact.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Contact;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $table = 'contacts';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'subject_area',
+        'earliest_start_year',
+        'currentsituation',
+    ];
+}
---
+++ resources/views/Emails/contact.blade.php
@@ -0,0 +1,9 @@
+<div>
+    
+   <h1>New Contact Form Submission</h1>
+   <p><strong>Name:</strong> {{ $details['name'] }}</p>
+   <p><strong>Email:</strong> {{ $details['email'] }}</p>
+   <p><strong>Subject Area:</strong> {{ $details['subject_area'] }}</p>
+   <p><strong>Earliest Start Year:</strong> {{ $details['earliest_start_year'] }}</p>
+   <p><strong>Current Situation:</strong> {{ $details['currentsituation'] }}</p>
+</div>
